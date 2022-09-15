b2c46a2ceaf317158cd8306dd74e06fe25db6a16
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-09-15 19:38:56 -0400

    tested notes working andd added to json array

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,29 @@
 const router = require('express').Router();
 const notesBase = require('../db/db.json')
+const fs = require('fs')
 "/notes"
+
 
 
 // GET route for retrieving notes
 router.get("/notes", (req, res) => {
     console.log(notesBase)
-    // res.json(notesBase)
+    // notesBase.push("fried pickles")
+    console.log(notesBase)
+    res.json(notesBase)
 
 })
 
+router.post("/notes", (req, res) => {
+    console.log(req.body)
+    notesBase.push(req.body)
 
+fs.writeFile('./db/db.json', JSON.stringify(notesBase), (err) => {
+    if (err) console.error(err) 
+
+    res.sendStatus(200)
+})
+})
 
 
 
